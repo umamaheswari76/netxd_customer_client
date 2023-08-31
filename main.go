4c703ab3648d014b53659b5570beda0f65af2910
umamaheswari76 <umamahes1301@gmail.com>
2023-08-31 11:28:42 +0530

    added client files

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	cst "netxd_project/netxd_project_proto_proto"
+
+	"google.golang.org/grpc"
+)
+
+func main(){
+
+	//Dial creates a client connection to the given target.
+	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	if err != nil{
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := cst.NewCustomerServiceClient(conn)
+
+	response, err := client.CreateCustomer(context.Background(), &cst.Customer{
+		CustomerId: 101,
+		FirstName:  "umamaheswari",
+		SecondName: "m",
+		BankId:     "1",
+		Balance:    5000,
+	})
+	if err!=nil{
+		log.Fatalf("Failed to call CreateCustomer: %v", err)
+	}
+
+	fmt.Printf("Response: %v\n", response.CustomerId)
+
+
+}
